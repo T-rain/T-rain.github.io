b3a928407ec9002df1bc9b54d0c124fd3cbe0a60
huang tsun yu <huangtsunyu@Travis-Huang.local>
2015-03-24 18:53:30 +0800

    homework-week4-1

---
+++ js/caculate.js
@@ -0,0 +1,27 @@
+function a() {
+  alert("d");
+}
+
+
+
+
+/*$(function() {
+
+  //用一個function包住同樣的程式碼
+
+
+
+  
+  var currentText = "";
+
+  $("btn1").on("click",function() {
+     alert("Hello");
+  })
+
+
+  $("message").text()
+
+
+  
+
+})*/
